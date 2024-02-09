1fbfce083a808785ab535d9c62b17f1e3c589117
Danil Gertsog <ger26dan@gmail.com>
2024-02-09 18:25:33 +0300

    feat: add limit to posts

--- app/store.ts
+++ app/store.ts
@@ -32,7 +32,7 @@ export const usePostsStore = create<PostsState>((set) => ({
     posts: [],
     fetchPosts: async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
         const result = await response.json();
         set({ posts: result });
       } catch (error) {
